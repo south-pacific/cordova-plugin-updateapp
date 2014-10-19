93e4571ffc1a7584dd1eca52dbcd32d653135a70
Robin Lei <robin.leibin@gmail.com>
2014-10-19 21:02:40 +0800

    IOS代码更新，使用线程方式

--- src/ios/CDVUpdateApp.m
+++ src/ios/CDVUpdateApp.m
@@ -7,17 +7,13 @@ NSString *ipaPath;
 
 - (void)getCurrentVersion:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* callbackId = command.callbackId;
-    NSString* version = [self getCurrentVersionCode];
-
-    CDVPluginResult* pluginResult = nil;
-    NSString* javaScript = nil;
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
-    javaScript = [pluginResult toSuccessCallbackString:callbackId];
-
-    [self writeJavascript:javaScript];
+    [self.commandDelegate runInBackground:^{
+        NSString* version = [self getCurrentVersionCode];
+        
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
+        
+        [self.commandDelegate evalJs:[pluginResult toSuccessCallbackString:command.callbackId]];
+    }];
 }
 
 - (void)getServerVersion:(CDVInvokedUrlCommand*)command
@@ -25,32 +21,43 @@ NSString *ipaPath;
     
     NSString *URL = [command argumentAtIndex:0];
     
-    NSDictionary *resultDic = [self getServerVersionCode:URL];
-    NSString *lastVersion = [resultDic objectForKey:@"verName"];
+    [self.commandDelegate runInBackground:^{
+        NSDictionary *resultDic = [self getServerVersionCode:URL];
         
-    NSString* callbackId = command.callbackId;
-        
-    CDVPluginResult* pluginResult = nil;
-    NSString* javaScript = nil;
-        
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:lastVersion];
-    javaScript = [pluginResult toSuccessCallbackString:callbackId];
-        
-    [self writeJavascript:javaScript];
+        if (resultDic == nil) {
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"error"];
+            
+            [self.commandDelegate evalJs:[pluginResult toErrorCallbackString:command.callbackId]];
+        } else {
+            NSString *lastVersion = [resultDic objectForKey:@"verName"];
+            
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:lastVersion];
+            
+            [self.commandDelegate evalJs:[pluginResult toSuccessCallbackString:command.callbackId]];
+        }
+    }];
 }
 
 - (void)checkAndUpdate:(CDVInvokedUrlCommand*)command
 {
     NSString *URL = [command argumentAtIndex:0];
-    NSDictionary *resultDic = [self getServerVersionCode:URL];
-    NSString *lastVersion = [resultDic objectForKey:@"verName"];
-    ipaPath = [resultDic objectForKey:@"ipaPath"];
     
-    if (![[self getCurrentVersionCode] isEqualToString:lastVersion]) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"更新" message:@"有新的版本，是否前往更新？" delegate:self cancelButtonTitle:@"关闭" otherButtonTitles:@"更新", nil];
-        alert.tag = 10000;
-        [alert show];
-    }
+    [self.commandDelegate runInBackground:^{
+        NSDictionary *resultDic = [self getServerVersionCode:URL];
+        
+        if (resultDic) {
+            NSString *lastVersion = [resultDic objectForKey:@"verName"];
+            ipaPath = [resultDic objectForKey:@"ipaPath"];
+            
+            if (![[self getCurrentVersionCode] isEqualToString:lastVersion]) {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"更新" message:@"有新的版本，是否前往更新？" delegate:self cancelButtonTitle:@"关闭" otherButtonTitles:@"更新", nil];
+                alert.tag = 10000;
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [alert show];
+                });
+            }
+        }
+    }];
 }
 
 - (NSString *)getCurrentVersionCode {
@@ -58,16 +65,22 @@ NSString *ipaPath;
 }
 
 - (NSDictionary *)getServerVersionCode:(NSString *)url {
-    NSError *error = nil;
     
-    //加载一个NSURL对象
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
-    //将请求的url数据放到NSData对象中
-    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-    //IOS5自带解析类NSJSONSerialization从response中解析出数据放到字典中
-    NSDictionary *resultDic = [NSJSONSerialization JSONObjectWithData:response options:NSJSONReadingMutableLeaves error:&error];
-    
-    return resultDic;
+    @try {
+        NSError *error = nil;
+        
+        //加载一个NSURL对象
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+        //将请求的url数据放到NSData对象中
+        NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        //IOS5自带解析类NSJSONSerialization从response中解析出数据放到字典中
+        NSDictionary *resultDic = [NSJSONSerialization JSONObjectWithData:response options:NSJSONReadingMutableLeaves error:&error];
+        
+        return resultDic;
+    }
+    @catch (NSException *exception) {
+        return nil;
+    }
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
